54ccd3d11a3d5d02f61177a0da494e4a0d00ac9d
fernandadutra <100439986+fernandadutra@users.noreply.github.com>
2023-08-20 10:43:16 -0300

    Add files via upload

---
+++ screens/Login.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {View,Text,StyleSheet} from "react-native";
+
+export default class Login extends Component{
+render(){
+    return(
+        <View>
+            <Text>Tela de Login</Text>
+        </View>
+    )
+}
+
+
+}
